58ce5cffecddbf3d312c2d9d7adc29032320a707
Murphy <thehelixpg@gmail.com>
2022-11-03 11:20:12 +0200

    added conditions props table

--- gui/card_widget/card_widget.py
+++ gui/card_widget/card_widget.py
@@ -2,11 +2,11 @@ from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QVBoxLayout, QFrame, QGroupBox, QSizePolicy
 
 
-class CardWidget(QGroupBox):
+class CardWidget(QFrame):
     def __init__(self, parent=None):
         super(CardWidget, self).__init__(parent)
         self.Layout = QVBoxLayout(self)
         self.Layout.setContentsMargins(0, 0, 0, 0)
         self.Layout.setSpacing(0)
         self.Layout.setAlignment(Qt.AlignTop | Qt.AlignHCenter)
-        self.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
+        self.setSizePolicy(QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed))
